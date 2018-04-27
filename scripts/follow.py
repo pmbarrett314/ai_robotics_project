bac6f3f07b12a26fdb8fcf8f505e7a72a62ef8b4
Paul Barrett <pmbarrett314@gmail.com>
2018-04-26 20:01:04 -0500

    simplified a lot

--- scripts/follow.py
+++ scripts/follow.py
@@ -3,21 +3,16 @@ import rospy
 from geometry_msgs.msg import *
 from sensor_msgs.msg import *
 from nav_msgs.msg import *
+from kobuki_msgs.msg import *
 import math
 import random
 
-##########################
-# BEGIN Global Variable Definitions
 front_distance = -1.0
 wall_distance = -1.0
-# END Global Variable Definitions
-##########################
 
-##########################
-# BEGIN ROS Topic Callback Function [DON'T MESS WITH THIS STUFF]
-##########################
 def laserCallback(laser_data):
-#This function will set front_distance and wall_distance.  Both of these work in the same way.  If something is closer than the detection_distance for that sensor, the value will tell how close something is.  If nothing is closer than the detection distance, the value of -1 will be used.
+#This function will set front_distance and wall_distance.  Both of these work in the same way. 
+#If something is closer than the detection_distance for that sensor, the value will tell how close something is.  If nothing is closer than the detection distance, the value of -1 will be used.
 
     global front_distance
     global wall_distance
@@ -51,14 +46,14 @@ def laserCallback(laser_data):
     if wf_min_right < wall_detection_distance:
         wall_distance = wf_min_right
 
-############################
-##### END CALLBACK FUNCTION
-############################
+def bumpCallback(bump):
+    print("bumped")
 
 if __name__ == '__main__':
     rospy.init_node('lab3', anonymous=True) #Initialize the ros node
     pub = rospy.Publisher('/cmd_vel_mux/input/teleop', Twist) #Create our publisher to send drive commands to the robot
     rospy.Subscriber("/scan", LaserScan, laserCallback) #Subscribe to the laser scan topic
+    rospy.Subscriber("/mobile_base/events/bumper",BumperEvent,bumpCallback)
 
     rate = rospy.Rate(10) #10 Hz
 
@@ -66,95 +61,17 @@ if __name__ == '__main__':
     global front_distance #How close is something in front of the robot (-1 = nothing is close)
     global wall_distance  #How close is something on the right side of the robot (-1 = nothing is close)
 
-    #########################################
-    # LAB 3 VARIABLE DECLARATION CODE : BEGIN
-    #########################################
-
-    # PART C CODE HERE:
-    #  Define and initialize variables that
-    #  you need inside the main loop
-
-
-    STATE_START="start"
-    STATE_TURN_LEFT_1="left 1"
-    STATE_FOLLOW_FORWARD="follow"
-    STATE_TURN_RIGHT="right"
-    STATE_TURN_LEFT_2="left 2"
-    state=STATE_START
-    #######################################
-    # LAB 3 VARIABLE DECLARATION CODE : END
-    #######################################
     while not rospy.is_shutdown():
-        print(state)
         print(front_distance)
         #Declare the twist command that we will publish this time step
         twist = Twist()
-        if front_distance>=0 or wall_distance>=0:
-		print("{} : {}".format(front_distance,wall_distance))
-        ###############################
-        # LAB 3 INNER LOOP CODE : BEGIN
-        ###############################
-        object_in_front=0<=front_distance<=0.5
-        wall_too_close=0<wall_distance<0.2
-        wall_ok=0.2<=wall_distance<=0.8
-        wall_too_far=0.8<wall_distance
-        wall=wall_too_close or wall_ok or wall_too_far
-        no_wall=wall_distance==-1
-        # PART C CODE HERE:
-        # Make sure that twist gets set with your drive command
-        if state==STATE_START:
-            if object_in_front:
-                state=STATE_TURN_LEFT_1
-            elif wall:
-                state=STATE_FOLLOW_FORWARD
-            else:
-                pass
-                #twist.linear.x = abs(front_distance)/10
-        elif state==STATE_TURN_LEFT_1:
-            if object_in_front:
-                twist.angular.z=math.pi/3
-            elif wall:
-                state=STATE_FOLLOW_FORWARD
-            else:
-                state=STATE_START
-        elif state==STATE_FOLLOW_FORWARD:
-            if object_in_front:
-                state=STATE_TURN_LEFT_2
-            elif wall:
-                diff=(0.5-wall_distance)
-                turn_factor=math.log(abs(diff)/0.5+1)
-                speed_factor=math.exp(-abs(diff)/0.5)
-                sign=0 if diff == 0 else diff/(abs(diff))
-                if wall_too_close:
-                    #twist.linear.x=0.2
-                    twist.angular.z=math.pi/6
-                elif wall_too_far:
-                    twist.linear.x=0.5
-                    twist.angular.z=-math.pi/8
-                else:
-                    #print("diff {} turn factor {}".format(diff,turn_factor))
-                    twist.angular.z=sign*turn_factor*math.pi/4
-                    twist.linear.x=speed_factor
-            else:
-                state=STATE_TURN_RIGHT
-        elif state==STATE_TURN_RIGHT:
-            if object_in_front:
-                state=STATE_TURN_LEFT_2
-            elif wall:
-                state=STATE_FOLLOW_FORWARD
-            else:
-                twist.angular.z=-math.pi/3
-                twist.linear.x=0.7
-        elif state==STATE_TURN_LEFT_2:
-            if object_in_front:
-                twist.angular.z=math.pi/3
-            elif wall:
-                state=STATE_FOLLOW_FORWARD
-            else:
-                state=STATE_TURN_RIGHT
-        #############################
-        # LAB 3 INNER LOOP CODE : END
-        #############################
+        
+        object_in_front=0<=front_distance<=0.65
+
+	if object_in_front:
+            twist.angular.z=-math.pi/2
+        else:
+            twist.linear.x=0.2
 
         #Publish drive command
         pub.publish(twist)
